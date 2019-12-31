1394a5b4c20ce434969e9d69f553db521e8826c5
Bán Dénes <mr@zealot.hu>
2019-12-31 14:53:28 +0100

    Added day of week to the clock popup

--- .config/i3/time.sh
+++ .config/i3/time.sh
@@ -1,7 +1,9 @@
-#! /bin/bash
+#! /bin/zsh
 
 # Send notification about current time and date
 
-date=$(date +"%F")
+date=$(date +"%y-%m-%d")
 time=$(date +"%H:%M")
-$HOME/.config/i3/notify.sh lookAtTime 4116302 clock "<span size=\"50000\">$time</span>\n<span size=\"25000\">$date</span>"
+days=('H' 'K' 'Sze' 'Cs' 'P' 'Szo' 'V')
+day=${days[`date +"%u"`]}
+$HOME/.config/i3/notify.sh lookAtTime 4116302 clock "<span size=\"50000\">$time</span>\n<span size=\"20000\">$date ($day)</span>"
